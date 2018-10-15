35fdb2df098d0c4b6a1f5203f2cf1e3c72381277
Maximillian Lowe <maxjlowe@gmail.com>
2018-10-15 14:27:13 -0500

    wrapped besides favicon

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,3 +21,30 @@ $(function() {
   });
   $('.open-menu').height($(window).height());
 });
+
+//Smooth Scrolling jquery plug in
+//.cf is reference to nav class="cf"
+// a is reference to a href
+//  hash is whatever  # nav point (music, gift cards, etc)
+// calls animate method, passing property of scrollTop, passing variable (hash).offset().top
+//800 is the speed of 800ms,
+//window.location.hash = hash; sets it to move to whatever hash
+//
+
+$('.cf a').on('click', function(event) {
+	if (this.hash !== '') {
+		event.preventDefault();
+
+		const hash = this.hash;
+
+		$('html, body').animate(
+			{
+				scrollTop: $(hash).offset().top
+			},
+			800,
+			function() {
+				window.location.hash = hash;
+			}
+		);
+	}
+});
